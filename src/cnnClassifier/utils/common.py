ca8b86d523ab7cf33bc9e61a6379f61276451635
ameyakonk <ameyakonkar@gmail.com>
2024-08-29 22:39:33 -0400

    (WIP) Created data ingestion pipeline

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -11,22 +11,22 @@ from typing import Any
 import base64
 
 
+
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
-    Args: 
-        path_to_yaml (str)" path like input
-    
+    Args:
+        path_to_yaml (str): path like input
+
     Raises:
         ValueError: if yaml file is empty
         e: empty file
-    
+
     Returns:
         ConfigBox: ConfigBox type
     """
-
-    try: 
+    try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
@@ -35,20 +35,22 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+    
+
 
 @ensure_annotations
-def create_directories(path_to_directories: list, verbose= True):
+def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
     Args:
-        path_to_directories (list): list of path of directories 
-        ignore_log(bool, optional): ignore if multiple dirs is to be created. Defaults to False
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
-
     for path in path_to_directories:
-        os.makedirs(path, exist_ok= True)
+        os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
+
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
@@ -58,15 +60,16 @@ def save_json(path: Path, data: dict):
         path (Path): path to json file
         data (dict): data to be saved in json file
     """
-
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
-    
+
     logger.info(f"json file saved at: {path}")
 
 
+
+
 @ensure_annotations
-def load_json(path: Path):
+def load_json(path: Path) -> ConfigBox:
     """load json files data
 
     Args:
@@ -75,12 +78,12 @@ def load_json(path: Path):
     Returns:
         ConfigBox: data as class attributes instead of dict
     """
-
     with open(path) as f:
-        content  = json.load(f)
+        content = json.load(f)
 
-    logger.info(f"json file loaded successfully from: {path}")
+    logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
+
 
 @ensure_annotations
 def save_bin(data: Any, path: Path):
@@ -90,17 +93,17 @@ def save_bin(data: Any, path: Path):
         data (Any): data to be saved as binary
         path (Path): path to binary file
     """
-
-    joblib.dump(value = data, filename = path)
+    joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at: {path}")
 
+
 @ensure_annotations
-def load_bin(path: Path):
-    """save binary file
+def load_bin(path: Path) -> Any:
+    """load binary data
 
     Args:
         path (Path): path to binary file
-    
+
     Returns:
         Any: object stored in the file
     """
